f8708a7bfd5dde15d98d4ea52115d2721da55ca0
Amirhossein Noori <nouri97dev@gmail.com>
2025-09-29 10:15:08 +0330

    feat : create separate components for each dashboard items

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { ServerStatusComponent } from "./server-status/server-status.component";
+import { TrafficComponent } from "./traffic/traffic.component";
+import { TicketsComponent } from "./tickets/tickets.component";
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [ServerStatusComponent, TrafficComponent, TicketsComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
