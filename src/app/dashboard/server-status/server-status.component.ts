46ddf79110b160bdba321c972c707ac8d9c749cb
Amirhossein Noori <nouri97dev@gmail.com>
2025-10-04 11:23:01 +0330

    feat: usage of effect

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -2,11 +2,13 @@ import {
   AfterViewInit,
   Component,
   DestroyRef,
+  effect,
   inject,
   Input,
   OnChanges,
   OnDestroy,
   OnInit,
+  signal,
   SimpleChanges,
 } from '@angular/core';
 
@@ -18,12 +20,18 @@ import {
   styleUrl: './server-status.component.css',
 })
 export class ServerStatusComponent implements OnInit, OnChanges, AfterViewInit {
-  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  // currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('offline');
   // @Input() test?: number;
   // private interval?: NodeJS.Timeout;
   // private interval?: ReturnType<typeof setInterval>;
   private destroyRef = inject(DestroyRef);
-  constructor() {}
+  constructor() {
+    effect(()=>{
+      console.log('effect : ',this.currentStatus());
+      
+    })
+  }
   ngOnInit() {
     console.log('run ng on init');
 
@@ -34,11 +42,12 @@ export class ServerStatusComponent implements OnInit, OnChanges, AfterViewInit {
       // console.log(rnd);
 
       if (rnd < 0.5) {
-        this.currentStatus = 'online';
+        // this.currentStatus = 'online';
+        this.currentStatus.set('online') ;
       } else if (rnd < 0.9) {
-        this.currentStatus = 'offline';
+        this.currentStatus.set('offline');
       } else {
-        this.currentStatus = 'unknown';
+        this.currentStatus.set('unknown') ;
       }
     }, 4000);
 
